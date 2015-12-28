8fb0a8334ea8996072414f3cde77b3688e12fdb4
lpy <clojurer@163.com>
2015-12-28 15:15:06 +0800

    去掉部分package，上传theme配置

--- init.el
+++ init.el
@@ -4,12 +4,23 @@
 (require 'init-elpa)
 
 (require 'init-dired)
-(require 'init-recentf)
 (require 'init-ido)
 (require 'init-editing-utils)
-(require 'init-evil)
 (require 'init-git)
-(require 'init-markdown)
 (require 'init-themes)
 (provide 'init)
 (put 'narrow-to-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
